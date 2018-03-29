2c45902822f88458f224b3103a6febc6e65eca36
zhangz14 <zhangz14@rose-hulman.edu>
2018-03-29 01:47:05 -0400

    11

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -324,9 +324,14 @@ def draw_circles(window, points, radius, color):
       :type color: str
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
+    for k in range(len(points)):
+        circle = rg.Circle(points[k], radius)
+        circle.fill_color = color
+        circle.attach_to(window)
+        window.render()
 
 
 # ----------------------------------------------------------------------
